9dea6ce26fb2347e9e6e4d450b027a2901428dc5
MacBook Pro <Nazaf@MacBook-Pro.local>
2021-07-01 12:54:35 +0600

    fixed share button position when window resized

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
-// Bubble margin adjust
-let bubble = document.getElementById('bubble-set');
-let bubble_width = bubble.offsetWidth;
-let margin_adjust = -bubble_width/2;
+console.log(window.innerWidth);
 
-let number = `${margin_adjust}px`;
+function onResize(){
+    let bubble = document.getElementById('bubble-set');
+    let share = document.getElementById('share-icon-container');
 
-bubble.style.marginRight = number;
 
-// Share icon margin adjust
-let share = document.getElementById('share-icon-container');
-let share_width = share.offsetWidth;
-let share_adjust = -share_width/2;
+    if (window.innerWidth > 1000){
+    
+        // Bubble margin adjust
+        let bubble_width = bubble.offsetWidth;
+        let margin_adjust = -bubble_width/2;
+    
+        let number = `${margin_adjust}px`;
+    
+        bubble.style.marginRight = number;
+    
+        // Share icon margin adjust
+        let share_width = share.offsetWidth;
+        let share_adjust = -share_width/2;
+    
+        let share_number = `${share_adjust}px`;
+    
+        share.style.marginRight = share_number;
+    } else if(window.innerWidth <= 1000){
+        bubble.style.marginRight = 0;
+        share.style.marginRight = 0;
 
-let share_number = `${share_adjust}px`;
+    }
+}
 
-share.style.marginRight = share_number;
+
+// Share button click event
+var bubbleSet = document.getElementById("bubble-set");
+
+var shareButton = document.getElementById("share-icon-container");
+
+
+function onClick() {
+    if(bubbleSet.className == 'bubble'){
+        bubbleSet.className = 'bubble-toggle';
+    } else if(bubbleSet.className == 'bubble-toggle'){
+        bubbleSet.className = 'bubble';
+    }
+}
 
